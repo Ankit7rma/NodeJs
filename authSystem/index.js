51845331af69b14c05aeae8fbb737d8840bfce10
Ankit Sharma <ankitsharma290101@gmail.com>
2025-04-26 15:11:37 +0530

    User Authentication System

--- authSystem/index.js
+++ authSystem/index.js
@@ -14,7 +14,7 @@ app.use(express.json())
 const users = []
 const tokenExpiredUsers = []
 
-const findUserByMail =(email)=> users.filter((user)=> user.email === email)
+const findUserByEmail = (email) => users.find((user) => user.email === email);
 
 const authMiddelware = (req,res,next)=>{
     const authHeader = req.headers['authorization']
@@ -39,8 +39,68 @@ const authMiddelware = (req,res,next)=>{
 
 }
 
+app.post("/signup",async(req,res)=>{
+    try {
+        const {username,email,password} = req.body
+        
+        if (!username || !email || !password) {
+            return res.status(400).json({ message: 'All fields are required' });
+          }
+        if (findUserByEmail(email)) {
+            return res.status(400).json({ message: 'Email already registered' });
+          }
+        const hashedPassword = await bcrypt.hash(password,10)
+        users.push({username,email,password:hashedPassword})
+        console.log(users)
+        return res.status(201).json({message:"User Registered Successfully "})
+    } catch (error) {
+        return res.status(500).json({message:'Server Error',error: error.message})
+    }
+})
 
+app.post("/signin", async(req,res)=>{
+ try {
+    const {email,password} = req.body
+    if(!email || !password){
+        return res.status(400).json({message:"Email and password are required"})
+    }
+    const user = findUserByEmail(email)
+    if(!user){
+        return res.status(401).json({message:"Invalid email or password"})
+    }
+    const isMatch = await bcrypt.compare(password,user.password)
+    if (!isMatch) {
+        return res.status(401).json({ message: 'Invalid email or password' });
+      }
+   const token = jwt.sign(
+    { email: user.email, username: user.username },
+    JWT_SECRET,
+    { expiresIn: "1h" }
+);
 
+    return res.status(200).json({message:"Login Successful ", token})
+
+ } catch (error) {
+    return res.status(500).json({message:"Server Error",error:error.message})
+ }
+})
+
+app.post('/logout', authMiddelware, (req, res) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader.split(' ')[1];
+   
+    tokenExpiredUsers.push(token);
+  
+    res.json({ message: 'Logout successful' });
+  });
+  
+ 
+  app.get('/profile', authMiddelware, (req, res) => {
+    res.json({
+      message: 'Profile accessed successfully',
+      user: { email: req.user.email, username: req.user.username }
+    });
+  });
 
 app.listen(port,(req,res)=>{
     console.log(`Server is Running on port: ${port}`)
