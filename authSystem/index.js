792765092752f3752f706703019c569316c454b2
Ankit Sharma <ankitsharma290101@gmail.com>
2025-04-26 14:15:22 +0530

    Auth Middelware

--- authSystem/index.js
+++ authSystem/index.js
@@ -7,7 +7,41 @@ dotenv.config()
 
 const app = express()
 const port = process.env.PORT || 3002 
+const JWT_SECRET =  process.env.JWT_SECRET 
+
+app.use(express.json())
+
+const users = []
+const tokenExpiredUsers = []
+
+const findUserByMail =(email)=> users.filter((user)=> user.email === email)
+
+const authMiddelware = (req,res,next)=>{
+    const authHeader = req.headers['authorization']
+    if(!authHeader){
+        return res.status(401).json({message:'Authorization header missing'})
+    }
+    const token  = authHeader.split(' ')[1]
+    if (!token) {
+        return res.status(401).json({ message: 'Token missing' });
+      }
+    
+    if (tokenExpiredUsers.includes(token)) {
+        return res.status(401).json({ message: 'Token has been invalidated' });
+    }
+    try {
+         const decoded = jwt.verify(token,JWT_SECRET)
+         req.user = decoded
+         next()
+    } catch (error) {
+        return res.status(401).json({message: `Invalid or Expired Token , Error is : ${error.message}` })
+    }
+
+}
+
+
+
 
 app.listen(port,(req,res)=>{
     console.log(`Server is Running on port: ${port}`)
-})
+}) 
